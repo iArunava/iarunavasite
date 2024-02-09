2a8ad0721ec311f7cbb49a559158ae486407fa8b
Arunava <learningdroidarunava@gmail.com>
2024-02-09 14:47:06 +0530

    updates

--- main.js
+++ main.js
@@ -15,42 +15,53 @@ experiences = [
 expduration = [
 'Jan 2023 - Present',
 //'Dec 2022 - Jan 2023',
-'Nov 2021 - Nov 2022',
+'Nov 2021 - Dec 2022',
 'Sep 2020 - Nov 2021',
-'Jul 2019 - Aug 2019',
+'Aug 2019 - Aug 2019',
 'Feb 2019 - Jul 2019',
 //'May 2019 - Jul 2019',
 //'Feb 2019 - March 2019',
 ]
 
 expdesp = [
-["Working on fixing existing production bugs and customer success. (github.com/arc-aviso)",
-"Implemented feature to help client switch to our platform.",
+["Managing and priotization of tickets for the dev team for completion of Sales Engagement Module.",
+"Implemented Aviso's Browser real-time Call and SMS/Email scheduling.",
+"Spearheading the backend for Aviso's Sales Engagement Module. A complete seperate module which is being built from scratch. Devliered the 1st phase And have received terrific positive feedback from internal and customer demos.",
+"Followed key Software Development Principles to modify legacy code that would allow future developers to make updates to the codebase efficiently as per needed requirement without spending lot of time going through everything.",
+"Implemented single-handedly REST API for Interactions Data consumption for the client for millions of rows consumption in seconds. Completed in a tight deadline. Enabled customer switch to our platform.",
+"Delivered Aviso's Actual View/Snapshot View Feature. (Single-handedly)",
+"Delivered key patches for production issues in strict timelines.",
+"Enabling new joiners to ramp up in the platform faster.",
+"Re-designed the insertion pipelines for reducing insertion time from 50s to almost 4s.",
 "Worked on Mongodb pipelines and Elasticsearch upgrades.",
-"Collaborated with different teams to move tickets to production.",
-"Programmed multiple bash and python scripts for automation based on requirements."],
+"Collaboration across teams to resolve disputes enabling faster shipment of tickets to production.",
+"Programmed multiple bash and python scripts for automation based on requirements.",
+"Activity can be checked at <a href='github.com/arc-aviso'>Github arc-aviso</a> and <a href='github.com/arcaviso'>Github arcaviso</a>"],
 
 //["Worked towards updating the search infrastructure entirely to optimize and get to almost real-time chat indexing in the app using Telegram APIs.",
-//"Worked with Python based asynchronous programming using asyncio/asyncpg."],
+//"Worked with Python based asynchronous programming using asyncio/asyncpg.",
+//"Worked on benchmarking different python postgres clients.",
+	//"Worked in a lot of Python async code. Primarily focused on the search engine that was powering supermind. The search engine would index conversation data from different web3 channels that is spread across telegram, discord and twitter. Discord and twitter were under implementation. "],
 
 [
 "First employee and helped build a usable product which helped acquire the first F500 paying customer.",
 "Built the beta version of the Ecocrumb ESG Management software from scratch.",
 "Worked on Frontend (Vue) and Backend (Django)",
 "Complete infrastructure management for the software on AWS.",
-"Scraped ESG standards data from websites, PDFs and Excel Sheets and built relationships out of it, and preparing and cleaning the data for use in the product.",
+"Scraped ESG standards data from websites, PDFs and Excel Sheets and built relationships out of it, and preparing and cleaning the data for use in the product. Data Engineering practices were used in the process.",
 "Database management (Postgres).",
-"End-to-End Automated Deployment Pipelines on Github Actions integrated with AWS Fargate.",
+"Single handedly built E2E Automated Deployment Pipelines on Github Actions integrated with AWS Fargate.",
+"Worked with Python based asynchronous programming using asyncio/asyncpg for Data consumption pipelines.",
 ],
 [
-"We worked on about 4-5 ideas pivoting fast and working towards building a successful startup. Due to numerous reasons the ideas didnt work out. Few things i worked on:",
-"Worked on the frontend(Vue) and backend(Flask) of our MLOps Platform ( Youtube )",
-"Managed a team of 3 engineers who helped in getting us to the MVP stage of the MLOps Platform. Demo",
+"We worked on about 4-5 ideas pivoting fast and working towards building a successful startup.",
+"Worked on the frontend(Vue) and backend(Flask) of our MLOps Platform ( <a href='https://youtu.be/iVw4MhCJWPA?si=qK7CfANJxCUhDDrA'>Youtube</a> )",
+"Managed a team of 3 engineers who helped in getting us to the MVP stage of the MLOps Platform.",
 "Conceptualizing the workings of an AR Glass [Glass]",
-"Built a robust edge detection algorithm without deep learning that would help the glass understand the world around. ( Youtube )",
-"Built other CV Algorithms that are robust and uses traditional CV Algorithms, not Deep Learning. ( Youtube )",
-"Worked on a real-time posture detection algorithm Real Time Posture Detection",
-"Built a RaspberryPi powered device that can sit in the front of the car and analyze the surroundings in almost real-time. ( Demo) ( Thermal Camera Demo )",
+"Built a robust edge detection algorithm without deep learning that would help the glass understand the world around. ( <a href='https://youtu.be/GB1GXk4pHZ0'>Youtube</a> and <a href='https://youtu.be/6zIVADWK7Sc'> Youtube </a> )",
+"Built other CV Algorithms that are robust and uses traditional CV Algorithms, not Deep Learning. ( <a href='https://youtube.com/shorts/ggIt3XaiSBo'>Youtube</a> )",
+"Worked on a real-time posture detection algorithm Real Time Posture Detection. ( <a href='https://youtu.be/Tew0XWnxhas'> Youtube </a>)",
+"Built a RaspberryPi powered device that can sit in the front of the car and analyze the surroundings in almost real-time. ( <a href='https://youtu.be/RYMiqRZGJ4o'>Demo</a> and <a href='https://youtu.be/2kLVIoAcMBk'>Thermal Camera Demo</a> )",
 "Worked towards a SaaS that analyzes statsical significance of the features of a dataset and returns in increasing order the list of features that effects a certain feature(KPI).",
 "Scraped different kinds of website and PDFs using som basic tools as well as Selenium and tesseract, for data collection.",
 
@@ -155,10 +166,10 @@ const articlecarouselindicatorblock = document.getElementById("article-carousel-
 const articleblock = document.getElementById("article-carousel-inner");
 
 articles = [
-['First Move with Move!', 'http://sharc.attim.in/2023/01/28/hello-move.html', 'assets/img/article_banners/move_header.png'],
-['DeFi - The future of Finance', 'http://sharc.attim.in/2023/01/23/defi.html', 'assets/img/article_banners/defi_article_banner_final2.png'],
-['What is NFT?', 'http://sharc.attim.in/2023/01/14/what-is-nft.html', 'assets/img/article_banners/nftgallery_oncyber_compressed.jpg'],
-['Web3 Gaming: The future of Gaming!', 'http://sharc.attim.in/2023/01/07/web3-gaming.html', 'assets/img/article_banners/web3gaming_header.jpg'],
+['First Move with Move!', 'https://iarunava.github.io/sharcblog/2023/01/28/hello-move.html', 'assets/img/article_banners/move_header.png'],
+['DeFi - The future of Finance', 'https://iarunava.github.io/sharcblog/2023/01/23/defi.html', 'assets/img/article_banners/defi_article_banner_final2.png'],
+['What is NFT?', 'https://iarunava.github.io/sharcblog/what-is-nft.html', 'assets/img/article_banners/nftgallery_oncyber_compressed.jpg'],
+['Web3 Gaming: The future of Gaming!', 'https://iarunava.github.io/sharcblog/2023/01/07/web3-gaming.html', 'assets/img/article_banners/web3gaming_header.jpg'],
 ]
 
 /*
@@ -191,3 +202,6 @@ for (var i = 0; i < articles.length; i++) {
 							</div>'
     articleblock.innerHTML += htmltoinsert
 }
+
+// Make all links open in new tab
+function externalLinks() {   for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {     var b = c[a];     b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")   } } ; externalLinks(); 
