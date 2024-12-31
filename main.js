f96af46bb1b613e6c870082a132659918fff4a75
Arunava <learningdroidarunava@gmail.com>
2024-12-31 14:19:24 +0530

    Updates

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const expblock = document.getElementById("experienceblock");
 const workblock = document.getElementById("work");
 const talkblock = document.getElementById("talkblock");
 const skillblock = document.getElementById("skillblock");
+const summaryblock = document.getElementById("summaryblock");
 
 experiences = [
 'Software Engineer, Aviso AI', 
@@ -70,27 +71,14 @@ expdesp = [
 	//"Worked in a lot of Python async code. Primarily focused on the search engine that was powering supermind. The search engine would index conversation data from different web3 channels that is spread across telegram, discord and twitter. Discord and twitter were under implementation. "],
 
 [
-"First employee and helped build a usable product which helped acquire the first F500 paying customer.",
-"Built the beta version of the Ecocrumb ESG Management software from scratch.",
-"Worked on Frontend (Vue) and Backend (Django)",
-"Complete infrastructure management for the software on AWS.",
-"Scraped ESG standards data from websites, PDFs and Excel Sheets and built relationships out of it, and preparing and cleaning the data for use in the product. Data Engineering practices were used in the process.",
-"Database management (Postgres).",
-"Single handedly built E2E Automated Deployment Pipelines on Github Actions integrated with AWS Fargate.",
-"Worked with Python based asynchronous programming using asyncio/asyncpg for Data consumption pipelines.",
+"Engineered and single-handedly built the entire backend and infrastructure to deliver the beta version of Ecocrumb, securing the first set of paying Fortune 500 clients, while ensuring the system was efficient for end-users and easy for future engineers to build upon.",
+"Built End-to-End Deployment Pipelines using Github Actions and AWS Fargate, to eliminate human interventions in deployment, and reduce deployment times by over an 80% or more.",
+"Wrote Automation Scripts and Data Pipelines, to scrape required data from 15+ sources including websites, PDFs and Excel Sheets and restructure them into a common format, which enabled customers to fill their ESG Reports across different standards atleast 50% faster."
 ],
 [
-"We worked on about 4-5 ideas pivoting fast and working towards building a successful startup.",
-"Worked on the frontend(Vue) and backend(Flask) of our MLOps Platform ( <a href='https://youtu.be/iVw4MhCJWPA?si=qK7CfANJxCUhDDrA'>Youtube</a> )",
-"Managed a team of 3 engineers who helped in getting us to the MVP stage of the MLOps Platform.",
-"Conceptualizing the workings of an AR Glass [Glass]",
-"Built a robust edge detection algorithm without deep learning that would help the glass understand the world around. ( <a href='https://youtu.be/GB1GXk4pHZ0'>Youtube</a> and <a href='https://youtu.be/6zIVADWK7Sc'> Youtube </a> )",
-"Built other CV Algorithms that are robust and uses traditional CV Algorithms, not Deep Learning. ( <a href='https://youtube.com/shorts/ggIt3XaiSBo'>Youtube</a> )",
-"Worked on a real-time posture detection algorithm Real Time Posture Detection. ( <a href='https://youtu.be/Tew0XWnxhas'> Youtube </a>)",
-"Built a RaspberryPi powered device that can sit in the front of the car and analyze the surroundings in almost real-time. ( <a href='https://youtu.be/RYMiqRZGJ4o'>Demo</a> and <a href='https://youtu.be/2kLVIoAcMBk'>Thermal Camera Demo</a> )",
-"Worked towards a SaaS that analyzes statsical significance of the features of a dataset and returns in increasing order the list of features that effects a certain feature(KPI).",
-"Scraped different kinds of website and PDFs using som basic tools as well as Selenium and tesseract, for data collection.",
-
+"Designed and Engineered a MLOps platform backend (Flask) and frontend (Vue), leading a team of 3 engineers to achieve MVP delivery which received strong positive feedback from 10+ angel investors and pre-seed funding."
+"Built a Raspberry Pi-powered device for real-time environmental analysis in day as well as night using Thermal Camera and prototyped a SaaS platform for statistical feature analysis of datasets.",
+"Designed End-to-End Automated Pipelines for High Quality Video Generation for Youtube utilizing freely available video and audio data on the internet, <strong>reducing video generation times by over a 95%</strong>."
 ],
 [
 "Worked on a paper which aims to do semi-supervised segmentation. We invented an algorithm that takes 5 paired images and learns how to perform proper segmentation with just 5 images and then applied that to the unseen data to do proper segmentation for all other images.",
@@ -98,12 +86,8 @@ expdesp = [
 "Pointer for the interested: https://arxiv.org/abs/2010.12622",
 ],
 [
-"Worked with the team in building articles that helps spread awareness and helps newbies getting into the field of computer vision get their feet wet.,",
-"Link: https://learnopencv.com/pytorch-for-beginners-semantic-segmentation-using-torchvision/",
-"Worked in creating content and code for course content for the 3 major courses that was released by OpenCV in the year 2021.",
-"Worked on Projects focused on Segmentation",
-"Reproduced a paper to solve a business problem by Semantic Segmentation.",
-"Used EAST for text Detection on Articles.",
+"Developed course code and techincal content for OpenCV’s Computer Vision and Deep Learning courses, focusing on segmentation algorithms",
+"Published an article on Semantic Segmentation using PyTorch - [OpenCV] and implemented text detection algorithms using EAST and reproduced multiple other DL/ML Papers."
 ],
 //[
 //"Worked on Projects focused on Segmentation",
@@ -331,6 +315,20 @@ for (var i = 0; i < skill_categories.length; i++) {
     htmltoinsert+='</div>'
     skillblock.innerHTML += htmltoinsert
 }
+
+
+/*
+ * Summary
+ *
+ * */
+
+const professional_summary = "Highly skilled Software Engineer with expertise in backend development, system architecture, and cloud technologies. Proven track record in leading engineering projects, driving revenue growth, and delivering high-impact solutions. Experienced in developing scalable applications, designing APIs, and automating processes. Recognized for strong technical leadership and problem-solving abilities."
+
+htmltoinsert = '<div><p> ' + professional_summary + ' </p> </div>'
+summaryblock.innerHTML += htmltoinsert
+
+
+
 
 
 // Make all links open in new tab
